b86b686592168fddc8058b0c2cd9487425a90d5d
UMESH KUMAR <UMESH_223722@SAITM.org>
2024-10-04 21:28:28 +0530

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Button } from "../components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>welcom to my website </h2>
+      <Button>hydh</Button>
+    </div>
+  );
+}
