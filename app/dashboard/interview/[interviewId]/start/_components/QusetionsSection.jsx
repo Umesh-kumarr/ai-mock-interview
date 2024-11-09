4f335dda0262091a45573a490f0f04b90fde6b86
UMESH KUMAR <UMESH_223722@SAITM.org>
2024-11-09 21:23:34 +0530

    added QusetionsSection

---
+++ app/dashboard/interview/[interviewId]/start/_components/QusetionsSection.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function QusetionsSection({mockInterviewQuestion}) {
+  return (
+    <div className='p-5 border rounded-lg'>
+        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
+            {mockInterviewQuestion&&mockInterviewQuestion.map((question,index)=>(
+                <h2 className='P-2 bg-secondary rounded-full'>Question #{index+1}</h2>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default QusetionsSection
