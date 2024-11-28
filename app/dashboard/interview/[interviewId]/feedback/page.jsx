11ae3526f8229744f5a6f6150ef4115f9e6a30e9
UMESH KUMAR <UMESH_223722@SAITM.org>
2024-11-28 18:01:59 +0530

    feedback page

--- app/dashboard/interview/[interviewId]/feedback/page.jsx
+++ app/dashboard/interview/[interviewId]/feedback/page.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
+import { db } from '/utils/db'
+import { eq } from 'drizzle-orm'
+import { UserAnswer } from '/utils/schema'
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "/components/ui/collapsible"
 
-function Feedback() {
+
+
+function Feedback({params}) {
+
+const [feedbackList,setFeedbackList] =useState([]);
+
+useEffect(()=>{
+  GetFeedback();
+},[])
+
+const GetFeedback=async()=>{
+  const result=await db.select()
+  .from(UserAnswer)
+  .where(eq(UserAnswer.mockIdRef,params.interviewId))
+  .orderBy(UserAnswer.id);
+
+  console.log(result);
+  setFeedbackList(result);
+}
+
   return (
-    <div>Feedback</div>
+    <div className='p-10'>
+      <h2 className='text-3xl font-bold text-green-500'>Congratulation!</h2>
+      <h2 className='font-bold text-2xl'>Here is your interview feedback</h2>
+      <h2 className='text-primary text-lg my-3'>Your overall interview rating: <strong>7/10</strong></h2>
+
+      <h2 className='text-sm text-gery-500'>Find below interview questions with correct answer, and feedback for improvement</h2>
+      {feedbackList&&feedbackList.map((item,index)=>(
+        <Collapsible key={index}>
+        <CollapsibleTrigger>Can I use this in my project?</CollapsibleTrigger>
+        <CollapsibleContent>
+          Yes. Free to use for personal and commercial projects. No attribution
+          required.
+        </CollapsibleContent>
+      </Collapsible>
+      
+      ))}
+    </div>
   )
 }
 
